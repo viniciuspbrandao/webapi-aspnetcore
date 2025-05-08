e1be3eab3addd9e9a178ab90e4ee12bdcd5ed7a6
viniciuspbrandao <brandao.vinicius@outlook.com>
2025-05-08 15:15:49 -0300

    Marcando ponto de funcionamento no envio de json

---
+++ WebApi/Infra/ConnectionContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApi.Model;
+
+namespace WebApi.Infra
+{
+    public class ConnectionContext : DbContext
+    {
+        public DbSet<Employee> Employees { get; set; }
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+            => optionsBuilder.UseMySQL(
+                "Server=localhost;" +
+                "Port=3306;" +
+                "Database=exercicios_fixacao;" +
+                "User=root;" +
+                "Password=root;"
+                );
+
+
+    }
+}
---
+++ WebApi/Model/Employee.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WebApi.Model
+{
+    [Table("employee")]
+    public class Employee
+    {
+        [Key]
+        public int id {  get; private set; }
+        public string name { get; set; }
+        public int age { get; set; }
+        public string photo { get; set; }
+
+        public Employee(string name, int age, string photo)
+        {
+            this.name = name ?? throw new ArgumentNullException(nameof(name));
+            this.age = age;
+            this.photo = photo;
+        }
+    }
+}
